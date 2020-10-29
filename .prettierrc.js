ce4d6e2e57cfa81c7763ee5a382c1d049f713115
root <root@debian>
2020-10-29 01:21:57 +0000

    feat: Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+const altergoPrettierConfig = require('@altergo/prettier-config');
+
+module.exports = {
+  ...altergoPrettierConfig,
+};
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+
+import Application from './src/Application';
+import { name } from './app.json';
+
+AppRegistry.registerComponent(name, () => Application);
